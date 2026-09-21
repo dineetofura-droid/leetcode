56aca9da5277ca9db04d7cb68398d57fd2b9f4e6
dineetofura-droid <dineetofura@gmail.com>
2026-09-21 14:39:08 +0530

    Time: 5 ms (83.47%) | Memory: 45.8 MB (71.82%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int n=x, rev=0;
+        
+        while(x>0){
+            rev=rev*10+x%10;
+            x=x/10;
+        }
+        
+        return n==rev;
+    }
+}
