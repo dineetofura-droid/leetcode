cfe8361d4d02c8e101b41524bd14c3f977771494
dineetofura-droid <dineetofura@gmail.com>
2026-09-22 13:40:53 +0530

    Time: 1 ms (99.93%) | Memory: 45.1 MB (88.98%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int compress(char[] chars) {
+        int write = 0;
+        int i = 0;
+
+        while (i < chars.length) {
+            char ch = chars[i];
+            int count = 0;
+
+            while (i < chars.length && chars[i] == ch) {
+                i++;
+                count++;
+            }
+
+            chars[write++] = ch;
+
+            if (count > 1) {
+                String s = String.valueOf(count);
+
+                for (char c : s.toCharArray()) {
+                    chars[write++] = c;
+                }
+            }
+        }
+
+        return write;
+    }
+}
